e4f221282e175949c8488f03c384cac071d63ee2
Erick Bogarin <erick.bogarin@gmail.com>
2018-10-12 16:02:52 -0400

    Server Side GrapQL

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,23 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
 
-module.exports = mutations;
+    return item;
+  }
+  //   createDog(parent, args, ctx, info) {
+  //     global.dogs = global.dogs || [];
+  //     console.log(global.dogs);
+  //     const newDog = { name: args.name };
+  //     global.dogs.push(newDog);
+  //     return newDog;
+  //   }
+};
+
+module.exports = Mutations;
